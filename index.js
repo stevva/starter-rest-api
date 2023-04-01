5f98a1c8030e22dc1972043bd0d4ec7d19cc1874
stevva <46354728+stevva@users.noreply.github.com>
2023-04-02 01:56:36 +0200

    no-cors test 1

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use('/:scores', async (req, res) => {
   const parsedScoreBoardFromDB = scoreBoardFromDB
     ? JSON.parse(JSON.stringify(scoreBoardFromDB, null, 2)).props.scoreBoardItems
     : []
-  const scoreBoardFromUser = req.body.scoreBoardFromUser
+  const scoreBoardFromUser = req.body
+  console.log('scoreBoardFromUser:', scoreBoardFromUser)
   const minScoreFromDb = parsedScoreBoardFromDB.reduce((acc, scoreEntry) => scoreEntry.score < acc ? scoreEntry.score : acc, Infinity)
   const validScoreEntriesFromUser = scoreBoardFromUser.filter(scoreEntryFromUser => {
     const isHighscore = scoreEntryFromUser.score > minScoreFromDb
