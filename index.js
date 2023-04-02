abf53a82030d986d8cfe101cdf7a2e7d0580e0ce
stevva <46354728+stevva@users.noreply.github.com>
2023-04-02 08:03:09 +0200

    using bodyParser

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const cors = require('cors');
 const db = require('@cyclic.sh/dynamodb')
-var http = require('http');
+const bodyParser = require("body-parser");
 
 const app = express()
 
 app.use(cors({ origin: '*' }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
 // Get scores from user and DB, join data and write joined to DB and send to user
 app.use('/:scores', async (req, res) => {
