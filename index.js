5d5f161f2ce9cf88a8b34d44172fbf4f59bf256a
stevva <46354728+stevva@users.noreply.github.com>
2023-04-02 08:04:44 +0200

    tweaked body-parser

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express()
 
 app.use(cors({ origin: '*' }))
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
